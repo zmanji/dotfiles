c3e09b65acc9e517e7f9898a877dc069dc3ddcf2
Zameer Manji <zmanji@gmail.com>
2014-04-08 00:31:34 -0400

    Disable the startup screen.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,3 +5,4 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq inhibit-startup-screen t)
