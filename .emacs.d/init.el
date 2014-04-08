3d65c8b95780a89a9d72b7d4a204808faf97ca0f
Zameer Manji <zmanji@gmail.com>
2014-04-08 00:28:34 -0400

    Disable unused GUI components.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,7 @@
 (when (< emacs-major-version 24)
   (error "Need Emacs v24 or higher. Currently have: v%d" emacs-major-version))
+
+; Remove unused GUI components.
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
