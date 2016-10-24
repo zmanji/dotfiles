228877c513a4ce28b9da5eafba1da76d705ba398
Zameer Manji <zmanji@gmail.com>
2016-10-23 17:19:19 -0700

    Disable hyper mode for real.

--- karabiner.sh
+++ karabiner.sh
@@ -4,16 +4,6 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set parameter.keyoverlaidmodifier_timeout 800
 /bin/echo -n .
-$cli set private.hyper_chrome 1
-/bin/echo -n .
-$cli set private.hyper_hints 1
-/bin/echo -n .
-$cli set private.hyper_iterm 1
-/bin/echo -n .
-$cli set private.hyper_key 1
-/bin/echo -n .
-$cli set private.hyper_rtm 1
-/bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
 $cli set repeat.initial_wait 200
