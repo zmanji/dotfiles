a54fb38060a892acc3bd517d707c26423eb6ec65
Zameer Manji <zmanji@gmail.com>
2017-06-28 13:59:49 -0700

    Add Automatic karabiner profile switching.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -65,9 +65,17 @@ hs.hotkey.bind(window_mash, "F", function()
   win:setFrame(f, 0)
 end)
 
-noop = function()
+-- http://www.hammerspoon.org/docs/hs.usb.watcher.html
+swich_karabiner_profile = function(usb_table)
+  -- Sculpt Keyboard IDs
+  if usb_table.vendorID == 1118 and usb_table.productID == 1957 then
+    if usb_table.eventType == 'added' then
+      output, status, ty, rc = hs.execute("/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile 'MSFT Sculpt'")
+    elseif usb_table.eventType == 'removed' then
+      output, status, ty, rc = hs.execute("/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile 'Default'")
+    end
+  end
 end
 
-hs.urlevent.bind("hints", function(eventName, params)
-  hs.hints.windowHints(nil, noop, true)
-end)
+watcher = hs.usb.watcher.new(swich_karabiner_profile)
+watcher:start()
