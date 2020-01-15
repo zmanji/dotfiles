444e69bfa89d2e07e2d903d20fa5239bb27cc818
Zameer Manji <zmanji@gmail.com>
2020-01-15 10:54:37 -0500

    Remove Sculpt Support

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -90,18 +90,3 @@ nil,
 function ()
   switcher:previous()
 end)
-
--- http://www.hammerspoon.org/docs/hs.usb.watcher.html
-swich_karabiner_profile = function(usb_table)
-  -- Sculpt Keyboard IDs
-  if usb_table.vendorID == 1118 and usb_table.productID == 1957 then
-    if usb_table.eventType == 'added' then
-      output, status, ty, rc = hs.execute("/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile 'MSFT Sculpt'")
-    elseif usb_table.eventType == 'removed' then
-      output, status, ty, rc = hs.execute("/Library/Application\\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile 'Default'")
-    end
-  end
-end
-
-watcher = hs.usb.watcher.new(swich_karabiner_profile)
-watcher:start()
