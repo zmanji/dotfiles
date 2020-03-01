82b792590c3e76481e11b080a76c6e6c3c41b54f
Zameer Manji <zmanji@gmail.com>
2020-03-01 16:26:53 -0500

    more surfingkeys

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -13,6 +13,7 @@ map('K', 'R')
 map('H', 'S')
 map('L', 'D')
 map('i', '<Alt-i>')
+map('F', 'gf')
 
 unmapAllExcept(['i', 'f', 'J', 'K', '<Esc>'], /messenger.com/)
 unmapAllExcept(['i', 'f', 'J', 'K', '<Esc>'], /mail.google.com/)
