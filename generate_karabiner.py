78136892cb2d0090d6ed7719c0a4d682742d4bf2
Zameer Manji <zmanji@gmail.com>
2021-04-15 22:54:08 -0400

    WIP

--- generate_karabiner.py
+++ generate_karabiner.py
@@ -3,44 +3,43 @@
 import os
 import json
 import collections
-from dataclasses import dataclass, asdict, is_dataclass
+from dataclasses import dataclass, asdict, is_dataclass, fields as datafields
 
 # https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/
 
-
-@dataclass
+@dataclass(frozen=True)
 class Modifiers:
     mandatory: list[str]
     optional: list[str]
 
 
-@dataclass
+@dataclass(frozen=True)
 class FromKey:
     key_code: str
     modifiers: list[Modifiers]
 
 
-@dataclass
+@dataclass(frozen=True)
 class SetVariable:
     name: str
     value: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class ToKey:
     key_code: str = None
     shell_command: str = None
     modifiers: list[str] = None
 
 
-@dataclass
+@dataclass(frozen=True)
 class Manipulator:
     _from: FromKey
     to: ToKey
     _type: str = "basic"
 
 
-@dataclass
+@dataclass(frozen=True)
 class Rule:
     manipulators: list[Manipulator]
     description: str
@@ -50,16 +49,18 @@ cmd_shift_mods = Modifiers(mandatory=["command"], optional="shift")
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
-
-        print(repr(obj))
-        if isinstance(obj, Manipulator):
-            print("ZAMEER WAS HERE")
-            d = asdict(obj, dict_factory=collections.OrderedDict)
-            d = OrderedDict(("from" if k == "_from" else k, v) for k, v in d.items())
-            return OrderedDict(("type" if k == "_type" else k, v) for k, v in d.items())
-        # elif is_dataclass(obj):
-        #     return asdict(obj, dict_factory=collections.OrderedDict)
-        return json.JSONEncoder.default(self, obj)
+        if is_dataclass(obj):
+            d = dict()
+            for f in datafields(obj):
+                name = f.name
+                if name == "_from":
+                    name = "from"
+                elif name == "_type":
+                    name = "type"
+                d[name] = getattr(obj, f.name)
+            return self.default(d)
+        else:
+            return super().default(obj)
 
 
 def generate_cmd_window_switch():
@@ -82,14 +83,14 @@ def generate_rules():
 def main():
     # read the existing config
     with open(os.path.expanduser("~/.config/karabiner/karabiner.json"), "r") as cfgfile:
-        cfg = json.load(cfgfile, object_pairs_hook=collections.OrderedDict)
+        cfg = json.load(cfgfile)
     # generate the rules
     rules = generate_rules()
     # swap the rules into the config
     cfg["profiles"][0]["complex_modifications"]["rules"] = rules
     # write out the config
     print("// generated from generate_karabiner.py")
-    print(json.dumps(cfg, indent=4, cls=Encoder))
+    print(json.dumps(cfg, cls=Encoder))
 
 
 if __name__ == "__main__":
