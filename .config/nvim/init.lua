2bd3270c4804287f8a28391743a45f5177974f88
Zameer Manji <zmanji@gmail.com>
2022-01-17 03:06:36 +0000

    add goop

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,12 +14,10 @@ vim.opt.background='dark'
 vim.cmd('colorscheme base16-tomorrow-night')
 -- }}}
 
-
 -- Plugins {{{
 vim.api.nvim_set_keymap('n', '<Leader>u', ':UndotreeToggle<CR>', {noremap = true})
 
 vim.api.nvim_set_keymap('n', '<Leader>bd', ':Bdelete<CR>', {noremap = true})
-
 
 -- Startify {{{
 vim.g.startify_custom_header = {}
@@ -36,6 +34,8 @@ require('nvim-treesitter.configs').setup {
                 enable = true,
         },
 }
+
+require('spellsitter').setup {}
 
 vim.cmd([[
 set foldmethod=expr
@@ -85,6 +85,86 @@ require('neogit').setup {}
 
 vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit<cr>', {noremap = true})
 
+vim.cmd [[
+	let g:sneak#label = 1
+]]
+
+vim.opt.completeopt="menu,menuone,noselect"
+
+local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-k>'] = cmp.mapping.select_prev_item(),
+      ['<C-j>'] = cmp.mapping.select_next_item(),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' },
+    }, {
+      { name = 'buffer' },
+      { name = 'path' },
+      { name = 'spell' },
+      { name = 'look', keyword_length=2, option={convert_case=true, loud=true}},
+    })
+  })
+
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  --   capabilities = capabilities
+  -- }
+
 -- }}}
 
 -- Basic Options {{{
@@ -120,7 +200,8 @@ vim.cmd [[
 ]]
 
 -- Custom dictionary
-vim.opt.spellfile = "~/.vim/custom-dictionary.en.utf8.add"
+vim.opt.spellfile = vim.env["HOME"] .. "/.vim/custom-dictionary.en.utf8.add"
+vim.opt.spell = true
 -- }}}
 
 -- Backups and Undo {{{
